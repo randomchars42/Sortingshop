94063c578094ac8a11993cf85fdb4c8405903002
randomchars42 <randomchars42@users.noreply.github.com>
2019-10-15 13:13:32 +0200

    configure rotatingfilehandler but don't use it

--- src/sortingshop/log/log.py
+++ src/sortingshop/log/log.py
@@ -4,27 +4,38 @@ config = {
     'version': 1,
     'disable_existing_loggers': True,
     'formatters': {
+        'detailed': {
+            'format': '%(asctime)s %(levelname)s %(module)s %(message)s',
+        },
         'simple': {
-            'format': '%(asctime)s %(module)s %(levelname)s %(message)s'
+            'format': '%(levelname)s %(module)s %(message)s',
         },
     },
     'handlers': {
         'console':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
-            'formatter': 'simple'
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
         },
+        'file':{
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'log',
+            'formatter': 'detailed',
+            'maxBytes': 1024,
+            'backupCount': 5,
+            }
     },
     'loggers': {
         '__main__': {
             'handlers': ['console'],
             'propagate': False,
-            'level':'DEBUG',
+            'level': 'DEBUG',
         },
         'sortingshop': {
             'handlers': ['console'],
             'propagate': False,
-            'level':'DEBUG',
+            'level': 'DEBUG',
         },
         'sortingshop.ui': {
             'handlers': ['console'],

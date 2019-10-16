76039acd415035d7291394b5db2505fee2dc37f5
randomchars42 <randomchars42@users.noreply.github.com>
2019-10-16 10:11:02 +0200

    use exiftool from src in exiftool-src

--- src/sortingshop/sortingshop.py
+++ src/sortingshop/sortingshop.py
@@ -2,6 +2,9 @@
 
 import logging
 import logging.config
+import pkg_resources
+
+from pathlib import Path
 
 from . import exiftool
 from .ui import ui
@@ -12,12 +15,25 @@ logger = logging.getLogger(__name__)
 class Sortingshop():
     def __init__(self):
         logger.info('init')
+        et = exiftool.ExifToolSingleton()
+        print(et.do('-ver')['text'])
         nui = ui.UI()
 
 def main():
     logging.config.dictConfig(log.config)
 
-    with exiftool.ExifToolSingleton():
+    if pkg_resources.resource_exists(__name__, 'exiftool-src/exiftool'):
+        executable=[
+                pkg_resources.resource_filename(__name__, 'exiftool-src') +
+                '/exiftool']
+    elif Path('/usr/bin/exiftool').exists():
+        executable=['/usr/bin/exiftool']
+        logger.warning('No ExifTool executable detected at ' +
+                '"src/sortingshop/exiftool-src/exiftool".')
+    else:
+        raise FileNotFoundError('No ExifTool executable detected.')
+
+    with exiftool.ExifToolSingleton(executable=executable):
         sosho = Sortingshop()
 
 if __name__ == '__main__':

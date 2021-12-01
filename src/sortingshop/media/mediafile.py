bf1fa17a4cfbfc24075d29335897b04e9f153f0f
randomchars42 <randomchars42@users.noreply.github.com>
2021-12-01 22:30:04 +0100

    fix failing to go to next image sometimes

--- src/sortingshop/media/mediafile.py
+++ src/sortingshop/media/mediafile.py
@@ -343,7 +343,7 @@ class MediaFile(metadatasource.MetadataSource):
         # of use_sidecar
         if apply_default_tagset and not tagsets is None:
             logger.debug('applying default tags')
-            default_tagset = tagset.get_default_tagset()
+            default_tagset = tagsets.get_default_tagset()
             if len(default_tagset) > 0:
                 self.get_primary_source().toggle_tags(default_tagset,
                         tagsets=tagsets, force="in")
